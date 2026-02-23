fb1a485997150285952f9a36e25bf8ab9ec23ffc
TakatoraSama <weeraphan2545@gmail.com>
2026-02-23 21:45:38 +0700

    add keyword data

--- Scripts/KeywordDatabase.gd
+++ Scripts/KeywordDatabase.gd
@@ -2,33 +2,87 @@ class_name KeywordDatabase
 
 const KEYWORDS = {
 	"Fearsome": {
-		"Description": "This can't be targeted by enemy with 3 less Power.",
+		"Description": "{Play} skill from enemy units with less than 3 Power cannot target this unit.",
+		"Sprite": "",
 		"Stackable": false,
 		"Generatable": true,
-		"Positive": true
+		"Positive": true 
 	},
 	"Barrier": {
-		"Description": "Negates the next kill effect from the enemy. Persists until next round ends.",
+		"Description": "Negates the next {Kill} effect from the enemy. Remove this keyword at the next round end.",
+		"Sprite": "",
 		"Stackable": false,
 		"Generatable": true,
 		"Positive": true
 	},
 	"Regeneration": {
-		"Description": "Round End: Cleanse the permanent negative Power debuff.",
+		"Description": "{Round End}: Cleanses the permanent negative Power debuff.",
+		"Sprite": "",
 		"Stackable": false,
 		"Generatable": true,
 		"Positive": true
 	},
 	"Tough": {
 		"Description": "Reduces the number of negative Power debuffs received from all sources by 1.",
+		"Sprite": "",
 		"Stackable": false,
 		"Generatable": true,
 		"Positive": true
 	},
 	"Challenger": {
-		"Description": "On Summon: If enemy played a card here this turn, +2 Power.",
+		"Description": "While this keyword is active, +2 Power if enemy plays a card here. Inactivate this keyword at round end.",
+		"Sprite": "",
 		"Stackable": false,
 		"Generatable": true,
 		"Positive": true
+	},
+	"Lifesteal": {
+		"Description": "When this unit decreases other cards' Power, grant this Power that much.",
+		"Sprite": "",
+		"Stackable": false,
+		"Generatable": true,
+		"Positive": true
+	},
+	"Augmented": {
+		"Description": "When you play a created card, grant me +1 Power.",
+		"Sprite": "",
+		"Stackable": false,
+		"Generatable": true,
+		"Positive": true
+	},
+	"Elusive": {
+		"Description": "While this keyword is active, you can {swap} my lane. Inactivate this keyword once you swap.",
+		"Sprite": "",
+		"Stackable": false,
+		"Generatable": true,
+		"Positive": true
+	},
+	"Evolve": {
+		"Description": "I have +2 once you've had units with 4+ other unique positive keywords in play this game.",
+		"Sprite": "",
+		"Stackable": false,
+		"Generatable": true,
+		"Positive": true
+	},
+	"Spirit": {
+		"Description": "This unit has +1 Power. This keyword can stack.",
+		"Sprite": "",
+		"Stackable": true,
+		"Generatable": true,
+		"Positive": true
+	},
+	"Impact": {
+		"Description": "This lane has +1 Power. This keyword can stack.",
+		"Sprite": "",
+		"Stackable": true,
+		"Generatable": true,
+		"Positive": true
+	},
+	"Stun": {
+		"Description": "Stunned units cannot activate their Round End or Round Start effects. Remove this keyword at round end.",
+		"Sprite": "",
+		"Stackable": false,
+		"Generatable": false,
+		"Positive": false
 	}
 }
---
+++ Scripts/VocabDatabase.gd
@@ -0,0 +1,32 @@
+class_name VocabDatabase
+
+const VOCABS = {
+	"Play": {
+		"Description": "This card skill activates when you play a card from your hand.",
+	},
+	"Aura": {
+		"Description": "This card skill always effects while the unit is on the field.",
+	},
+	"GameStart": {
+		"Description": "This card skill activates at the start of the game.",
+	},
+	"GameEnd": {
+		"Description": "This card skill activates if this is in play at the end of the game.",
+	},
+	"RoundStart": {
+		"Description": "This card skill activates if this is in play at the start of each round.",
+	},
+	"RoundEnd": {
+		"Description": "This card skill activates if this is in play at the end of each round.",
+	},
+	"Kill": {
+		"Description": "Remove a unit. It goes to killed_cards array.",
+	},
+	"Discard": {
+		"Description": "Remove a card from your hand. It goes to discarded_cards array.",
+	},
+	"Swap": {
+		"Description": "Move a unit to a different lane.",
+	},
+
+}
